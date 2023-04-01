9dd004f7c9d374c84afff8c4315572f4876c9155
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2023-04-01 13:19:40 -0300

    Create Exercicio_0.c

---
+++ Prática 0/Exercicio_0.c
@@ -0,0 +1,11 @@
+/*==============================================================================
+PRÁTICA 0 - EXERCÍCIO 0: Imprima "Alo mundo!" na tela
+Made by Stéphanie Pereira Barbosa 01/04/2023
+==============================================================================*/
+
+#include <stdio.h>
+
+void main() 
+{
+  printf("Alo mundo!");
+}
