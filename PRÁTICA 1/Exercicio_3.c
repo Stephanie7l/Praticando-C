4804c5bee12ae9c36f4607ebbff56510d3bcce4c
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2023-04-01 13:30:15 -0300

    Create Exercicio_3.c

---
+++ PRÁTICA 1/Exercicio_3.c
@@ -0,0 +1,20 @@
+/*==============================================================================
+PRÁTICA 1 - EXERCÍCIO 3: Imprima na tela os senos de 3,14 e de 4,13
+Made by Stéphanie Pereira Barbosa on 01/04/2023
+==============================================================================*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main() 
+{
+  float x;
+  x = sin(3.14);
+  printf("Seno de 3,14 = %f", x);
+  printf("\n");
+  
+  float y;
+  y = sin(4.13);
+  printf("Seno de 4,13 = %f", y);
+  printf("\n");
+}
