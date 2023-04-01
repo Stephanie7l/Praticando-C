26157f59352ba2fec1e3b5dac8c17fe230c21fb0
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2023-04-01 13:29:07 -0300

    Create Exercicio_4.c

---
+++ PRÁTICA 1/Exercicio_4.c
@@ -0,0 +1,12 @@
+/*==============================================================================
+PRÁTICA 1 - EXERCÍCIO 4: Imprima na tela todos os arquivos da pasta
+Made by Stéphanie Pereira Barbosa on 01/04/2023
+==============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() 
+{
+  printf("%s", system("dir"));
+}
