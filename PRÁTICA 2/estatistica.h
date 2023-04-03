795259570b029034ef43163a39d126e89772a3ce
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2023-04-03 10:55:14 -0300

    Create estatistica.h

---
+++ PRÁTICA 2/estatistica.h
@@ -0,0 +1,9 @@
+/*==============================================================================
+PRÁTICA 2: Crie e compile um módulo que contenha as funções estatisticas 
+implementadas.
+Made by Stéphanie Pereira Barbosa on 01/04/2023
+==============================================================================*/
+
+float CAUCHY(float x);
+float GUMBEL(float x, float mi, float beta);
+float LAPLACE(float x, float mi, float beta);
