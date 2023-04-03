232461d37a01dabaf49dd88a6c5826b8dc625aae
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2023-04-03 10:58:45 -0300

    Update estatistica.h

--- PRÁTICA 2/estatistica.h
+++ PRÁTICA 2/estatistica.h
@@ -1,7 +1,7 @@
 /*==============================================================================
 PRÁTICA 2: Crie e compile um módulo que contenha as funções estatisticas 
 implementadas.
-Made by Stéphanie Pereira Barbosa on 01/04/2023
+Made by Stéphanie Pereira Barbosa on 03/04/2023
 ==============================================================================*/
 
 float CAUCHY(float x);
